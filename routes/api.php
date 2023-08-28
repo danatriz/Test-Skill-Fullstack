7726ee9f7e36df0c38989999b4cb517d1e3d95ec
Dana Triz <github@danatriz.com>
2023-08-29 04:24:18 +0700

    update vue

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,42 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\AuthController;
+use App\Http\Controllers\API\V1\ProfileController;
+use App\Http\Controllers\API\V1\ProductController;
+use App\Http\Controllers\API\V1\TransactionController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// ! Version 1 API
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // * Authentication
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    // ! Sanctum Middleware
+    Route::middleware('auth:sanctum')->group(function () {
+        // * Profile
+        Route::get('/profile', [ProfileController::class, 'index']);
+        Route::post('/profile', [ProfileController::class, 'update']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        // * Products
+        Route::get('products', [ProductController::class, 'index']);
+        Route::get('products/{product}', [ProductController::class, 'show']);
+        Route::post('products', [ProductController::class, 'store']);
+        Route::put('products/{product}', [ProductController::class, 'update']);
+        Route::delete('products/{product}', [ProductController::class, 'destroy']);
+
+        // * Transactions
+        Route::get('transactions', [TransactionController::class, 'index']);
+        Route::post('transactions', [TransactionController::class, 'store']);
+        Route::get('transactions/{transaction}', [TransactionController::class, 'show']);
+        Route::put('transactions/{transaction}', [TransactionController::class, 'update']);
+        Route::delete('transactions/{transaction}', [TransactionController::class, 'destroy']);
+    });
+});
+
+// ! Version 2 API (if needed)
+Route::prefix('v2')->group(function () {
+    // TODO: Will be added if needed
 });
