996b3ff37056b67f5529436409eee2899f4bd02a
Dana Triz <github@danatriz.com>
2023-08-29 03:47:30 +0700

    update dependencies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             userSeeder::class,
             ProductSeeder::class,
-            TransactionSeeder:class,
+            TransactionSeeder::class,
         ])
     }
 }
