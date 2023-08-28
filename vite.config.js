43327ed0c943e037ebe32df3664baf7c63a1bcbc
Dana Triz <github@danatriz.com>
2023-08-29 06:21:29 +0700

    update tampilan

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
         vue({
             template: {
                 transformAssetUrls: {
@@ -16,5 +12,25 @@ export default defineConfig({
                 },
             },
         }),
+        laravel({
+            input: [
+                "resources/css/app.css",
+                "resources/assets/css/icons.css",
+                "resources/js/app.js",
+                "resources/assets/libs/simplebar/simplebar.min.js",
+                "resources/assets/libs/chart.js/chart.min.js",
+                "resources/assets/libs/apexcharts/apexcharts.min.js",
+                "resources/assets/libs/echarts/echarts.min.js",
+                "resources/assets/js/main.js",
+            ],
+            refresh: true,
+        }),
     ],
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+            "@assets": "/resources/assets",
+            "@images": "/resources/assets/images",
+        },
+    },
 });
